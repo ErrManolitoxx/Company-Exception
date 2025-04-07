221af1f9e3f5d5f02b4d5a06a232b901b129dfe9
Allan <allanglopez1312@gmail.com>
2025-04-07 10:20:12 +0200

    Terminar Reader de departamento

---
+++ src/main/java/org/example/components/Random/RandomDepartmentScanner.java
@@ -0,0 +1,13 @@
+package org.example.components.Random;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Random;
+
+@AllArgsConstructor
+public class RandomDepartmentScanner {
+
+    private final Random random;
+
+
+}
