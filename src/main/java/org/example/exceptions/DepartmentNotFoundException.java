588117df6946093cbbb77954104add4e05f232a1
Allan <allanglopez1312@gmail.com>
2025-04-08 11:14:24 +0200

    Excepciones hechas con mensaje

--- src/main/java/org/example/exceptions/DepartmentNotFoundException.java
+++ src/main/java/org/example/exceptions/DepartmentNotFoundException.java
@@ -1,5 +1,17 @@
 package org.example.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+@Getter
+@EqualsAndHashCode(callSuper = true)
 public class DepartmentNotFoundException extends Exception{
+    private String name;
+
+    public DepartmentNotFoundException(String message, String name) {
+        super("No se ha encontrado el departamanto " + name);
+        this.name = name;
+    }
 }
--- src/main/java/org/example/exceptions/EmployeeNotFoundException.java
+++ src/main/java/org/example/exceptions/EmployeeNotFoundException.java
@@ -1,5 +1,14 @@
 package org.example.exceptions;
 
-public class EmployeeNotFoundException extends Exception {
+import lombok.*;
 
+@Getter
+@EqualsAndHashCode(callSuper = true)
+public class EmployeeNotFoundException extends Exception {
+    private String nif;
+
+    public EmployeeNotFoundException(String message, String nif) {
+        super("No se ha podido encontrar el empleado con el NIF " + nif);
+        this.nif = nif;
+    }
 }
