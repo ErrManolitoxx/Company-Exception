72ead73aa0510e1dc53fe7ceb57200979d8e89bb
Allan <allanglopez1312@gmail.com>
2025-04-08 11:15:02 +0200

    Primer metodo de company hecho

--- src/main/java/org/example/model/Company.java
+++ src/main/java/org/example/model/Company.java
@@ -2,14 +2,26 @@ package org.example.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.extern.log4j.Log4j;
+import org.example.exceptions.DepartmentNotFoundException;
 
-import java.util.TreeSet;
+import java.util.List;
+import java.util.Map;
 
 @Data
+@Log4j
 @AllArgsConstructor
 public class Company {
 
     private String name;
     private String CIF;
-    private TreeSet<Department> departments;
+    private Map<String, Department> departmentsByName;
+
+    public void showDepartments() {
+        departmentsByName.values().forEach(log::info);
+    }
+
+    public List<Employee> findEmployeesDepartment() {
+
+    }
 }
